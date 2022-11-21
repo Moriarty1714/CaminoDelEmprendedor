9a6fd61e4105eee1daa7d1c379105dff0c510baa
Marti De Ferrer Fuertes <marti.deferrer@enti.cat>
2022-11-21 22:48:25 +0100

    Splash screen Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image ludensLogo;
+    public Image acehLogo;
+    public Image splashScreenPanel;
+
     public TextMeshProUGUI nameUser;
     public TextMeshProUGUI email;
     public TextMeshProUGUI business;
@@ -13,13 +18,17 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FadeImage(ludensLogo,0, false));
+        StartCoroutine(FadeImage(acehLogo, 0, false));
+        StartCoroutine(FadeImage(ludensLogo, 2, true));
+        StartCoroutine(FadeImage(acehLogo, 2, true));
+        StartCoroutine(FadeImage(splashScreenPanel,3, true));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SaveInfo() {
@@ -41,5 +50,32 @@ public class MainMenu : MonoBehaviour
 
     public void sceneGame() {
         SceneManager.LoadScene("Game");
+    }
+
+    IEnumerator FadeImage(Image img,int seconds, bool fadeAway)
+    {
+        yield return new WaitForSeconds(seconds);
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(img.color.r, img.color.g, img.color.b, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(img.color.r, img.color.g, img.color.b, i);
+                yield return null;
+            }
+        }
     }
 }
